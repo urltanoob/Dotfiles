21205b814f319fe3263a004c68bcc8e573b034df
Jamison <jBiberdorf937@gmail.com>
2026-03-25 14:07:42 -0600

    Completed wallpaper.sh with style that follows pywal, added new version of doom.jpg.

--- modules/home/scripts/wallpaper.sh
+++ modules/home/scripts/wallpaper.sh
@@ -2,14 +2,14 @@
 WALLPAPER_DIR="$HOME/nixos-config/wallpapers/"
 
 menu() {
-	find "${WALLPAPER_DIR}" -type f \( -iname "*.jpg" -o -iname "*jpeg" -o -iname "*.png" -o -iname "*.gif" \) | awk '{print "img:"$0}'
+	find "${WALLPAPER_DIR}" -type f \( -iname "*.jpg" -o -iname "*jpeg" -o -iname "*.png" -o -iname "*.gif" \) | sort | awk '{print "img:"$0}'
 }
 
 main() {
 	choice=$(menu | wofi --show dmenu --prompt "Select Wallpaper:" -n -c ~/nixos-config/modules/home/wofi/wallpaper -s ~/nixos-config/modules/home/wofi/style-wallpaper.css)
 	selected_wallpaper=$(echo "$choice" | sed 's/^img://')
-	swww img "$selected_wallpaper" --transition-type wipe  --transition-duration .5 
-	wal -i "$selected_wallpaper"
+	wal -i "$selected_wallpaper" -n
+	swww img "$selected_wallpaper" --transition-type wipe --transition-fps 60 --transition-duration 1
 }
 
 main
