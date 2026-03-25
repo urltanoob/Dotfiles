2c38f5b5d824ee758192df3004ef19f0d2e3987b
Jamison <jBiberdorf937@gmail.com>
2026-03-25 07:12:22 -0600

    Puter ssd mounts.

--- hosts/puter/configuration.nix
+++ hosts/puter/configuration.nix
@@ -4,4 +4,16 @@
   imports = [
     ./hardware-configuration.nix
   ];
+
+  fileSystems."/mnt/Gamering" = {
+    device = "/dev/disk/by-uuid/36AA56A2AA565E85";
+    fsType = "ntfs-3g";
+    options = [ "uid=1000" "gid=100" "dmask=022" "fmask=133" "nofail" ];
+  };
+
+  fileSystems."/mnt/Less Gamering" = {
+    device = "/dev/disk/by-uuid/6E0A20A10A2067F3";
+    fsType = "ntfs-3g";
+    options = [ "uid=1000" "gid=100" "dmask=022" "fmask=133" "nofail" ];
+  };
 }
