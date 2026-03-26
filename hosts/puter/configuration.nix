8b8963f01b32cf551c68bbbeb8a03181cc83c16f
Jamison <jBiberdorf937@gmail.com>
2026-03-26 09:22:28 -0600

    Changed mount group for puter drives to 1000 from 100.

--- hosts/puter/configuration.nix
+++ hosts/puter/configuration.nix
@@ -8,12 +8,12 @@
   fileSystems."/mnt/Gamering" = {
     device = "/dev/disk/by-uuid/36AA56A2AA565E85";
     fsType = "ntfs-3g";
-    options = [ "uid=1000" "gid=100" "dmask=022" "fmask=022" "exec" "nofail" ];
+    options = [ "uid=1000" "gid=1000" "dmask=022" "fmask=022" "exec" "nofail" ];
   };
 
   fileSystems."/mnt/Less Gamering" = {
     device = "/dev/disk/by-uuid/6E0A20A10A2067F3";
     fsType = "ntfs-3g";
-    options = [ "uid=1000" "gid=100" "dmask=022" "fmask=022" "exec" "nofail" ];
+    options = [ "uid=1000" "gid=1000" "dmask=022" "fmask=022" "exec" "nofail" ];
   };
 }
